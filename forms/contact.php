ff31005e80b49634b9cb482c00de2f2684a0d2a2
Shahd Abass <shahdabass94@gmail.com>
2024-09-27 18:35:15 +0300

    push

---
+++ forms/contact.php
@@ -0,0 +1,42 @@
+<?php
+
+require('../assets/vendor/smtp/PHPMailer.php');
+require('../assets/vendor/smtp/SMTP.php');
+require('../assets/vendor/smtp/Exception.php');
+
+ use PHPMailer\PHPMailer\PHPMailer;
+ use PHPMailer\PHPMailer\SMTP;
+ use PHPMailer\PHPMailer\Exception;
+ $mail = new PHPMailer(true);
+
+ try {
+   //Server settings
+   //$mail->SMTPDebug = SMTP::DEBUG_SERVER;          
+   $mail->isSMTP();     
+   $mail->SMTPAuth   = true;                       
+   $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+   $mail->Port       = 587;        
+   $mail->SMTPSecure = "tls";                  
+
+   $mail->Host       = 'smtp.gmail.com';
+   $mail->Username   = 'sajelha.sa@gmail.com';
+   $mail->Password   = 'K5Vs3Znb3SW3pI2H';
+   
+   $mail->setFrom('sajelha.sa@gmail.com', 'Dawen');
+   $mail->addAddress('sajelha.sa@gmail.com');
+
+   $mail->isHTML(true);
+   $mail->Subject = 'Notification: Received New Inquiry!';
+   $mail->Body    = 'Name: ' . $_POST['name'] . '<br>' . 
+                    'Email: ' . $_POST['email'] . '<br>'. 
+                    'Subject: ' . $_POST['subject'] . '<br>' .
+                    'Message: '. $_POST['message'];
+
+     $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent.";
+}
+ 
+ 
+?>
